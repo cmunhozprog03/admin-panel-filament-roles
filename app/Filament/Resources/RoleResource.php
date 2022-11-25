1de8d2cb3deb73a4f18a37f55a5a2389a8440a23
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-11-25 13:29:32 -0300

    Roles realtion permission

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -2,28 +2,47 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\RoleResource\Pages;
-use App\Filament\Resources\RoleResource\RelationManagers;
-use App\Models\Role;
 use Filament\Forms;
-use Filament\Resources\Form;
-use Filament\Resources\Resource;
-use Filament\Resources\Table;
 use Filament\Tables;
+
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use Spatie\Permission\Models\Role;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\RoleResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\RoleResource\RelationManagers;
+use App\Filament\Resources\RoleResource\RelationManagers\PermissionRelationManager;
+use App\Filament\Resources\RoleResource\RelationManagers\PermissionsRelationManager;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\MultiSelect;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
 
 class RoleResource extends Resource
 {
     protected static ?string $model = Role::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-s-cog';
+    protected static ?string $navigationGroup = 'Admin Manengent';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Card::make()
+                ->schema([
+                    TextInput::make('name')
+                        ->unique(ignoreRecord: true)
+                        ->required(),
+                        
+                    MultiSelect::make('permissions')
+                        ->relationship('permissions', 'name')
+                        ->preload()
+                        ->required()
+                ])
             ]);
     }
 
@@ -31,13 +50,21 @@ class RoleResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime('d/m/y')
+                    ->sortable()
+                    ->searchable()
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
@@ -45,9 +72,11 @@ class RoleResource extends Resource
     }
     
     public static function getRelations(): array
-    {
-        return [
-            //
+    { 
+       return [
+            //PermissionsRelationManager::class(),
+            PermissionsRelationManager::class,
+           
         ];
     }
     
